5823a63f6a19ed564bd2f7f8b33993a37e97137d
Yannick Pflanzer <yannick.pflanzer@scary-squid.de>
2014-06-24 14:29:21 +0200

    + More documentation

--- MSDK/MEngine/Includes/MScene.h
+++ MSDK/MEngine/Includes/MScene.h
@@ -27,10 +27,8 @@
 //
 //========================================================================
 
-
 #ifndef _M_SCENE_H
 #define _M_SCENE_H
-
 
 // data modes
 enum M_DATA_MODES
@@ -41,6 +39,13 @@ enum M_DATA_MODES
 };
 
 // MScene
+/**
+ * @brief The MScene class represents a scene in a Maratis level.
+ *
+ * One level can have multiple independent scenes with objects and scripts.
+ * You can use this to overlay a scene over another to create ingame GUI or similar.
+ *
+ */
 class M_ENGINE_EXPORT MScene
 {
 public:
@@ -82,7 +87,21 @@ private:
 public:
 
 	// name
+    /**
+     * @brief Sets the scene name.
+     *
+     * The scene name is a property by which you can find a particular scene.
+     *
+     * @see MLevel::getSceneByName
+     *
+     * @param name The new name which will be copied to an internal buffer.
+     */
 	void setName(const char * name);
+
+    /**
+     * @brief Returns the name of the scene.
+     * @return The scene name.
+     */
 	inline const char * getName(void){ return m_name.getSafeString(); }
 
 	// data mode
@@ -90,11 +109,40 @@ public:
 	inline M_DATA_MODES getDataMode(void){ return m_dataMode; }
 
 	// script
+    /**
+     * @brief Sets the scenes script that will be executed.
+     *
+     * Sets the path to a script. Those script can contain game logics.
+     *
+     * @param scriptFilename
+     */
 	void setScriptFilename(const char * scriptFilename);
+
+    /**
+     * @brief Returns the filename of the script that is currently attached to this scene.
+     * @return The path to the script.
+     */
 	inline const char * getScriptFilename(void){ return m_scriptFilename.getData(); }
 
 	// gravity
+    /**
+     * @brief Sets the gravitational pull in this scene to the given value.
+     *
+     * This new gravity is stored in a MVector3.
+     *
+     * Example:
+     * \code
+     * scene.setGravity(MVector3(0.0f, 0.0f, -9.81f));
+     * \endcode
+     *
+     * @param gravity
+     */
 	inline void setGravity(const MVector3 & gravity){ m_gravity = gravity; }
+
+    /**
+     * @brief Returns the current gravity in a MVector3
+     * @return The gravity vector.
+     */
 	inline MVector3 getGravity(void) const { return m_gravity; }
 
 	// sounds
@@ -102,12 +150,43 @@ public:
 	void stopAllSounds(void);
 
 	// delete object
+    /**
+     * @brief Removes the given object from this scene.
+     * @param object The object to be removed.
+     */
 	void deleteObject(MObject3d * object);
 
 	// add objects
+    /**
+     * @brief Creates a new camera, adds it to the scene and returns it.
+     *
+     * @see MCamera
+     *
+     * @return The new camera.
+     */
 	MOCamera * addNewCamera(void);
+
+    /**
+     * @brief Copies the camera and adds the new instance to the scene.
+     * @param camera The camera to copy.
+     * @return The new copy of the camera.
+     */
 	MOCamera * addNewCamera(const MOCamera & camera);
+
+    /**
+     * @brief Creates a new light source and adds it to the scene.
+     *
+     * @see MOLight
+     *
+     * @return The new light source.
+     */
 	MOLight * addNewLight(void);
+
+    /**
+     * @brief Copies the light and adds the new instance to the scene.
+     * @param light The light to copy from.
+     * @return The new copy.
+     */
 	MOLight * addNewLight(const MOLight & light);
 	MOEntity * addNewEntity(MMeshRef * meshRef);
 	MOEntity * addNewEntity(const MOEntity & entity);
@@ -117,11 +196,40 @@ public:
 	MOText * addNewText(const MOText & text);
 
 	// get objects number
+    /**
+     * @brief Returns the number of objects in the scene.
+     * @return Number of objects
+     */
 	inline unsigned int getObjectsNumber(void){ return m_objects.size(); }
+
+    /**
+     * @brief Returns the number of cameras in the scene.
+     * @return Number of cameras
+     */
 	inline unsigned int getCamerasNumber(void){ return m_cameras.size(); }
+
+    /**
+     * @brief Returns the number of lights in the scene.
+     * @return Number of lights
+     */
 	inline unsigned int getLightsNumber(void){ return m_lights.size(); }
+
+    /**
+     * @brief Returns the number of entities in the scene.
+     * @return Number of entities
+     */
 	inline unsigned int getEntitiesNumber(void){ return m_entities.size(); }
+
+    /**
+     * @brief Returns the number of sounds in the scene.
+     * @return Number of sounds
+     */
 	inline unsigned int getSoundsNumber(void){ return m_sounds.size(); }
+
+    /**
+     * @brief Returns the number of text objects in the scene.
+     * @return Number of text objects
+     */
 	inline unsigned int getTextsNumber(void){ return m_texts.size(); }
 
 	// current camera
