db9a7e929bc641a1380073f444242d5c84610522
Yannick Pflanzer <ypflanzer@outlook.com>
2014-09-11 21:24:29 +0200

    * Copy executables and libs to one directory on build

---
+++ CMake/CopyFiles.cmake
@@ -0,0 +1,17 @@
+macro(copy_files GLOBPAT DESTINATION TARGETNAME)
+  file(GLOB COPY_FILES
+    RELATIVE ${CMAKE_CURRENT_SOURCE_DIR}
+    ${GLOBPAT})
+  add_custom_target(${TARGETNAME} ALL
+    COMMENT "Copying files: ${GLOBPAT}")
+
+  foreach(FILENAME ${COPY_FILES})
+    set(SRC "${CMAKE_CURRENT_SOURCE_DIR}/${FILENAME}")
+    set(DST "${DESTINATION}/${FILENAME}")
+
+    add_custom_command(
+      TARGET ${TARGETNAME}
+      COMMAND ${CMAKE_COMMAND} -E copy ${SRC} ${DST}
+      )
+  endforeach(FILENAME)
+endmacro(copy_files)
