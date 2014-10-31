60848b68ca2e35834fd93c54ca864dd7e238f2bc
Yannick Pflanzer <yp1995@outlook.com>
2014-10-31 13:13:36 +0100

    + Added skeletton for the new NeoGui library.

---
+++ Neo/Gui/include/render.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2014 (C) Yannick Pflanzer <neo-engine.de>
+ *
+ * This file is part of NeoGui.
+ *
+ * NeoGui is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Diese Datei ist Teil von NeoGui.
+ *
+ * NeoGui ist Freie Software: Sie können es unter den Bedingungen
+ * der GNU Lesser General Public License, wie von der Free Software Foundation,
+ * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+ * veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ *
+ * NeoGui wird in der Hoffnung, dass es nützlich sein wird, aber
+ * OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+ * Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ * Siehe die GNU Lesser General Public License für weitere Details.
+ *
+ * Sie sollten eine Kopie der GNU Lesser General Public License zusammen mit diesem
+ * Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+ */
+
+namespace Neo
+{
+/**
+ * @brief The Render class contains helper methods for rendering 2D shapes.
+ *
+ * All elements can be rendered directly to the screen or to a texture.
+ */
+class Render
+{
+private:
+    Render m_instance;
+
+public:
+    Render* getInstance() { return &m_instance; }
+};
+}
---
+++ Neo/Gui/include/widget.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2014 (C) Yannick Pflanzer <neo-engine.de>
+ *
+ * This file is part of NeoGui.
+ *
+ * NeoGui is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Diese Datei ist Teil von NeoGui.
+ *
+ * NeoGui ist Freie Software: Sie können es unter den Bedingungen
+ * der GNU Lesser General Public License, wie von der Free Software Foundation,
+ * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+ * veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ *
+ * NeoGui wird in der Hoffnung, dass es nützlich sein wird, aber
+ * OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+ * Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ * Siehe die GNU Lesser General Public License für weitere Details.
+ *
+ * Sie sollten eine Kopie der GNU Lesser General Public License zusammen mit diesem
+ * Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+ */
+
+namespace Neo
+{
+/**
+ * @brief The Widget class contains all information that is common
+ * to all GUI widgets.
+ *
+ * Every object that resembles a GUI widget has to derive from this class.
+ */
+class Widget
+{
+public:
+
+};
+}
+
