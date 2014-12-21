bb4a282fc58674a4461bdd87de17f537268a220f
Yannick Pflanzer <yp1995@live.de>
2014-12-21 23:00:40 +0100

    * Animation switching test

--- Tests/LuaApi/scripts/LuaApiSDKTest.lua
+++ Tests/LuaApi/scripts/LuaApiSDKTest.lua
@@ -11,11 +11,21 @@ local tile = Tile(100,100,72,97,spriteSheet,0,0)
 
 mainCanvas:addWidget(tile)
 
-local walkAnimation = SpriteAnimation(0,3,150,true)
-
-tile:attachAnimation(walkAnimation)
+local walkAnimation = SpriteAnimation(0,3,70,true)
+local idleAnimation = SpriteAnimation(0,0,70,false)
 
 function onSceneUpdate()
     sprite:rotate(2)
+
+    if isKeyPressed("RIGHT") then
+	tile:attachAnimation(walkAnimation)
+	tile:translate(2,0)
+    elseif isKeyPressed("LEFT") then
+	tile:attachAnimation(walkAnimation)
+	tile:translate(-2,0)
+    else
+	tile:attachAnimation(idleAnimation)
+    end
+
     tile:update()
 end
