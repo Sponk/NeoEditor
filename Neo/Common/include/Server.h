6f7a5b880dd664a8fd7e700930c118de50233682
Yannick Pflanzer <yp1995@live.de>
2014-12-30 09:40:48 +0100

    * Cleaned up NeoCommon and added some license headers

--- Neo/Common/include/Server.h
+++ Neo/Common/include/Server.h
@@ -1,14 +1,38 @@
+//========================================================================
+// Copyright (c) 2014-2015 Yannick Pflanzer <neo-engine.de>
+//
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgment in the product documentation would
+//    be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such, and must not
+//    be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source
+//    distribution.
+//
+//========================================================================
+
 #ifndef __SEVER_H__
 #define __SEVER_H__
 
 #ifdef USE_NETWORKING
 
 #include <NeoEngine.h>
-#include <Client.h>
 #include <map>
 #include <string>
 
 #include <RakPeerInterface.h>
+#include <MessageIdentifiers.h>
 
 namespace Neo
 {
@@ -21,7 +45,6 @@ enum NetworkMessages
 class Server
 {
 private:
-	std::vector<Client> m_clients;
 	std::map<std::string, void (*)(RakNet::BitStream*)> m_rpcFunctions;
 	MThread* m_serverThread;
 
