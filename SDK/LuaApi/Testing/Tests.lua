fb81ba4fbad2ea55028c8c09a8bdf70dbadabb5b
Yannick Pflanzer <yp1995@outlook.com>
2015-01-07 12:45:03 +0100

    * Fully implemented the OLight class with all tests and docs

--- SDK/LuaApi/Testing/Tests.lua
+++ SDK/LuaApi/Testing/Tests.lua
@@ -7,7 +7,9 @@ end
 
 function TestLight()
 
-  local light = OLight()  
+  local light = OLight()
+  
+  -- Test constructor
   assertNotNil(light)
   assertEquals(light:getPosition(), {0,0,0})
 
@@ -29,6 +31,36 @@ function TestLight()
   light:translate({0,0,1}, "local")
   assertVectorEquals(light:getPosition(), {0,0,-1}, 0.00001)
 
+  -- Test Light specifics
+  light:setIntensity(5)
+  assertEquals(light:getIntensity(), 5)
+  
+  light:setColor({1,1,1})
+  assertVectorEquals(light:getColor(), {1,1,1}, 0.001)
+  
+  light:setRadius(15)
+  assertEquals(light:getRadius(), 15)
+  
+  light:enableShadows()
+  assertEquals(light:hasShadows(), true)
+  
+  light:disableShadows()
+  assertEquals(light:hasShadows(), false)
+  
+  light:setShadowBlur(15)
+  assertEquals(light:getShadowBlur(), 15)
+  
+  light:setShadowBias(3)
+  assertEquals(light:getShadowBias(), 3)
+  
+  light:setSpotAngle(45)
+  assertEquals(light:getSpotAngle(), 45)
+  
+  light:makeDirectional()
+  assertEquals(light:getSpotAngle(), 0)
+  
+  light:makePointLight()
+  assertEquals(light:getSpotAngle(), 180)
 end
 
 function TestEntity()
