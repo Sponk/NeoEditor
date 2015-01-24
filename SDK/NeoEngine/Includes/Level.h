62929a7ac85591acdb7d399b48fd8665750e3ff0
Yannick Pflanzer <yp1995@outlook.com>
2015-01-24 14:46:12 +0100

    * More documentation for Neo::Level

--- SDK/NeoEngine/Includes/Level.h
+++ SDK/NeoEngine/Includes/Level.h
@@ -1,10 +1,6 @@
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-// MEngine
-// MLevel.h
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 //========================================================================
 // Copyright (c) 2003-2011 Anael Seghezzi <www.maratis3d.com>
+// Copyright (c) 2015 Yannick Pflanzer <www.neo-engine.de>
 //
 // This software is provided 'as-is', without any express or implied
 // warranty. In no event will the authors be held liable for any damages
@@ -28,13 +24,21 @@
 //========================================================================
 
 
-#ifndef _M_LEVEL_H
-#define _M_LEVEL_H
+#ifndef __LEVEL_H
+#define __LEVEL_H
 
 
 // level
 namespace Neo
 {
+/**
+ * @brief The Level class contains all scenes and assets that are loaded.
+ *
+ * When loading a level, all assets like textures, shaders, fonts, sounds and meshes are only loaded
+ * once into memory for a maximum of efficiency. As soon as an asset is not used anymore it
+ * will be unloaded and the RAM it used will be free'd what allows to clean up
+ * during runtime.
+ */
 class M_ENGINE_EXPORT Level
 {
 private:
@@ -95,31 +99,166 @@ public:
 	inline MDataManager * getTexturesAnimManager(void){ return &m_texturesAnimManager; }
 	inline MDataManager * getMaterialsAnimManager(void){ return &m_materialsAnimManager; }
 
+	/**
+	 * @brief Loads a font from a file and returns the new FontRef object.
+	 *
+	 * This method will never return NULL, even if the specified file does not exist
+	 * or can not be read!
+	 *
+	 * This means you have to ensure the file exists before calling this method!
+	 *
+	 * @param filename The path to the file.
+	 * @return The new FontRef object.
+	 */
 	FontRef * loadFont(const char * filename);
+
+	/**
+	 * @brief Loads a mesh from a file and returns the new MeshRef object.
+	 *
+	 * This method will never return NULL, even if the specified file does not exist
+	 * or can not be read!
+	 *
+	 * This means you have to ensure the file exists before calling this method!
+	 *
+	 * @param filename The file to load from.
+	 * @return The new MeshRef
+	 */
 	MeshRef * loadMesh(const char * filename, const bool preload = true);
 	ArmatureAnimRef * loadArmatureAnim(const char * filename, const bool preload = true);
 	TexturesAnimRef * loadTexturesAnim(const char * filename, const bool preload = true);
 	MaterialsAnimRef * loadMaterialsAnim(const char * filename, const bool preload = true);
+
+	/**
+	 * @brief Loads a texture from a file and returns the new TextureRef object.
+	 *
+	 * This method will never return NULL, even if the specified file does not exist
+	 * or can not be read!
+	 *
+	 * This means you have to ensure the file exists before calling this method!
+	 *
+	 * @param filename The file to load from.
+	 * @param mipmap Indicates if a mip map should be generated for this texture.
+	 * @return The new TextureRef
+	 */
 	TextureRef * loadTexture(const char * filename, const bool mipmap = true, const bool preload = true);
+
+	/**
+	 * @brief Loads a sound from a file and returns the new SoundRef object.
+	 *
+	 * This method will never return NULL, even if the specified file does not exist
+	 * or can not be read!
+	 *
+	 * This means you have to ensure the file exists before calling this method!
+	 *
+	 * @param filename The file to load from.
+	 * @return The new SoundRef
+	 */
 	SoundRef * loadSound(const char * filename, const bool preload = true);
 	ShaderRef * loadShader(const char * filename, M_SHADER_TYPES type);
 
 	// queue data updates
+	/**
+	 * @brief Appends a DataRef to the update queue.
+	 *
+	 * @param ref The DataRef object.
+	 */
 	void sendToUpdateQueue(MDataRef * ref);
+
+	/**
+	 * @brief Appends a DataRef to the clear queue.
+	 *
+	 * @param ref The DataRef object.
+	 */
 	void sendToClearQueue(MDataRef * ref);
+
+	/**
+	 * @brief Updates all DataRef's in the update queue.
+	 *
+	 * This will call DataRef::update for every object that is currently
+	 * in the queue. The queue will be emptied so new requests can
+	 * be handled.
+	 */
 	void updateQueueDatas(void);
+
+	/**
+	 * @brief Clear all DataRef's in the clear queue.
+	 *
+	 * This will call DataRef::clear for every object that is currently
+	 * in the queue. The queue will be emptied so new requests can
+	 * be handled.
+	 */
 	void clearQueueDatas(void);
 
 	// scenes
+	/**
+	 * @brief Changes the current scene to the scene with the given ID.
+	 *
+	 * The scene will not change straight away but it will be merely requested to
+	 * do so in one of the next frames.
+	 *
+	 * @param id The new scene ID
+	 */
 	void changeCurrentScene(unsigned int id); // does not change scene straight away, instead it requests to do it
+
+	/**
+	 * @brief Deletes the scene with the given ID.
+	 *
+	 * @param id The ID of the scene
+	 */
 	void deleteScene(unsigned int id);
+
+	/**
+	 * @brief Retrieves the index of the scene with the given name.
+	 *
+	 * The ID will be saved into the given pointer.
+	 *
+	 * @param name The name to search for
+	 * @param index The pointer to an integer to save the ID
+	 * @return Returns false if the scene can not be found.
+	 */
 	bool getSceneIndexByName(const char * name, unsigned int * index);
+
+	/**
+	 * @brief Creates a new scene and returns a pointer to it.
+	 * @return The new scene
+	 */
 	Scene * addNewScene(void);
+
+	/**
+	 * @brief Retrieves the scene with the given name.
+	 * @param name The name to search for
+	 * @return The scene
+	 */
 	Scene * getSceneByName(const char * name);
+
+	/**
+	 * @brief Returns the current main scene.
+	 * @return The current scene.
+	 */
 	Scene * getCurrentScene(void);
+
+	/**
+	 * @brief Changes the current scene.
+	 * @param id The ID of the new scene.
+	 */
 	void setCurrentSceneId(unsigned int id);
+
+	/**
+	 * @brief Returns the current scene ID
+	 * @return The ID of the current scene.
+	 */
 	inline unsigned int getCurrentSceneId(void){ return m_currentSceneId; }
+
+	/**
+	 * @brief Returns the number of scenes.
+	 * @return The number of scenes.
+	 */
 	inline unsigned int getScenesNumber(void){ return m_scenes.size(); }
+
+	/**
+	 * @brief Searches the scene with the given ID.
+	 * @return The searched scene object.
+	 */
 	inline Scene * getSceneByIndex(unsigned int id){ return m_scenes[id]; }
 };
 }
