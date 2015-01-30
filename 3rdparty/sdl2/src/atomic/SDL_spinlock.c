e5f23c0499c593d5a053103970242b3bdffa8343
Yannick Pflanzer <yp1995@outlook.com>
2015-01-30 11:03:31 +0100

    * Some threading build fixes

--- 3rdparty/sdl2/src/atomic/SDL_spinlock.c
+++ 3rdparty/sdl2/src/atomic/SDL_spinlock.c
@@ -18,6 +18,7 @@
      misrepresented as being the original software.
   3. This notice may not be removed or altered from any source distribution.
 */
+#include <pthread.h>
 #include "../SDL_internal.h"
 
 #if defined(__WIN32__) || defined(__WINRT__)
