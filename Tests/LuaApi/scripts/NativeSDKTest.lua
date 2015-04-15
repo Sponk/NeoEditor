740259b80f61640e6ae0f69d56978f05833f5e9a
Yannick Pflanzer <yp1995@live.de>
2015-04-15 17:58:49 +0200

    * Minor changes to the LuaApiTest

--- Tests/LuaApi/scripts/NativeSDKTest.lua
+++ Tests/LuaApi/scripts/NativeSDKTest.lua
@@ -4,7 +4,7 @@ local gui = NeoGui.GuiSystem.getInstance()
 gui:setEnabled(true)
 
 local button = NeoGui.ThemedButton(100, 100, 300, 30, "This is a button!")
-local label = NeoGui.Label(100, 200, 300, 30, "This is a label!")
+local label = NeoGui.Label(100, 200, 0, 30, "This is a label!")
 
 local labelhandle = gui:addWidget(label)
 local buttonhandle = gui:addWidget(button)
@@ -18,6 +18,8 @@ function buttonCallback()
 end
 button:setScriptCallback("buttonCallback")
 
+local lastDelta = 0.0
 function onSceneUpdate(delta)
-    label:setLabel(delta*1000)
+    label:setLabel("Delta: " .. delta*1000 .. "\nDelta Skew: " .. 100*(1000*lastDelta / 1000*delta) .. "%")
+    lastDelta = delta
 end
