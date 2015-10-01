cf66315c8a46ac415dcd5ca11fc0c3ac0b50e8f5
Yannick Pflanzer <yp1995@live.de>
2015-10-01 14:36:23 +0200

    * Implemented physics properties in the editor

--- Neo/Editor/assets/editor/dialogs/entity-editor.lua
+++ Neo/Editor/assets/editor/dialogs/entity-editor.lua
@@ -175,13 +175,215 @@ local dlg = {
       },
       
       [15] = {
-        name = "physicsPropertiesButton",
-        type = "Button",
+        name = "physicsEnabledCheckButton",
+        type = "CheckButton",
+        label = tr("Physically Active"),
         x = 10,
-        y = 185,
+        y = 170,
+        size = 15,
+        label = "",
+        inputType = NeoLua.DECIMAL_INPUT,
+        callback = "updateEntityConfig"
+      },
+      
+      [16] = {
+        name = "massLabel",
+        type = "Label",
+        x = 10,
+        y = 200,
+        w = 0,
+        h = 0,
+        label = tr("Mass:")
+      },
+      
+      [17] = {
+        name = "massInput",
+        type = "InputField",
+        x = 10,
+        y = 210,
         w = 280,
         h = 20,
-        label = tr("Physics Properties")--,
+	inputType = NeoLua.DECIMAL_INPUT,
+        label = "0",
+        callback = "updateEntityConfig"
+      },
+      
+      [18] = {
+        name = "ghostEnabledCheckButton",
+        type = "CheckButton",
+        label = tr("Ghost"),
+        x = 10,
+        y = 240,
+        size = 15,
+        label = "",
+        inputType = NeoLua.DECIMAL_INPUT,
+        callback = "updateEntityConfig"
+      },
+      
+      [19] = {
+        name = "frictionLabel",
+        type = "Label",
+        x = 10,
+        y = 270,
+        w = 0,
+        h = 0,
+        label = tr("Friction:")
+      },
+      
+      [20] = {
+        name = "frictionInput",
+        type = "InputField",
+        x = 10,
+        y = 280,
+        w = 280,
+        h = 20,
+	inputType = NeoLua.DECIMAL_INPUT,
+        label = "0",
+        callback = "updateEntityConfig"
+      },
+      
+      [21] = {
+        name = "restitutionLabel",
+        type = "Label",
+        x = 10,
+        y = 310,
+        w = 0,
+        h = 0,
+        label = tr("Restitution:")
+      },
+      
+      [22] = {
+        name = "restitutionInput",
+        type = "InputField",
+        x = 10,
+        y = 320,
+        w = 280,
+        h = 20,
+	inputType = NeoLua.DECIMAL_INPUT,
+        label = "0",
+        callback = "updateEntityConfig"
+      },
+      
+       [23] = {
+        name = "linearDampingLabel",
+        type = "Label",
+        x = 10,
+        y = 350,
+        w = 0,
+        h = 0,
+        label = tr("Linear Damping:")
+      },
+      
+      [24] = {
+        name = "linearDampingInput",
+        type = "InputField",
+        x = 10,
+        y = 360,
+        w = 280,
+        h = 20,
+	inputType = NeoLua.DECIMAL_INPUT,
+        label = "0",
+        callback = "updateEntityConfig"
+      },
+      
+      [25] = {
+        name = "angularDampingLabel",
+        type = "Label",
+        x = 10,
+        y = 390,
+        w = 0,
+        h = 0,
+        label = tr("Angular Damping:")
+      },
+      
+      [26] = {
+        name = "angularDampingInput",
+        type = "InputField",
+        x = 10,
+        y = 400,
+        w = 280,
+        h = 20,
+	inputType = NeoLua.DECIMAL_INPUT,
+        label = "0",
+        callback = "updateEntityConfig"
+      },
+      
+      [27] = {
+        name = "angularFactorLabel",
+        type = "Label",
+        x = 10,
+        y = 430,
+        w = 0,
+        h = 0,
+        label = tr("Angular Factor:")
+      },
+      
+      [28] = {
+        name = "angularFactorInput",
+        type = "InputField",
+        x = 10,
+        y = 440,
+        w = 280,
+        h = 20,
+	inputType = NeoLua.DECIMAL_INPUT,
+        label = "1",
+        callback = "updateEntityConfig"
+      },
+      
+      [29] = {
+        name = "linearFactorLabel",
+        type = "Label",
+        x = 10,
+        y = 470,
+        w = 0,
+        h = 0,
+        label = tr("Linear Factor:")
+      },
+      
+      [30] = {
+        name = "lfxInput",
+        type = "InputField",
+        x = 10,
+        y = 480,
+        w = 60,
+        h = 20,
+        label = "1",
+        inputType = NeoLua.DECIMAL_INPUT,
+        callback = "updateEntityConfig"
+      },
+      
+      [31] = {
+        name = "lfyInput",
+        type = "InputField",
+        x = 85,
+        y = 480,
+        w = 60,
+        h = 20,
+        label = "1",
+        inputType = NeoLua.DECIMAL_INPUT,
+        callback = "updateEntityConfig"
+      },
+      
+      [32] = {
+        name = "lfzInput",
+        type = "InputField",
+        x = 160,
+        y = 480,
+        w = 60,
+        h = 20,
+        label = "1",
+        inputType = NeoLua.DECIMAL_INPUT,
+        callback = "updateEntityConfig"
+      },
+      
+      {
+        name = "physicsConstraintsButton",
+        type = "Button",
+        x = 10,
+        y = 530,
+        w = 280,
+        h = 20,
+        label = tr("Physics Constraints")--,
         --callback = ""      
       }
     }
@@ -225,8 +427,38 @@ function entityEditor.setShownObject(objName)
   dlg["syInput"]:setLabel(round(scale.y, 3))
   dlg["szInput"]:setLabel(round(scale.z, 3))
   
+  local phys = entityEditor.entity:getPhysicsProperties()
+  dlg["physicsEnabledCheckButton"]:setValue(phys ~= nil)
+  
+  if phys ~= nil then
+	dlg["massInput"]:setLabel(round(phys:getMass(), 3))
+	dlg["ghostEnabledCheckButton"]:setValue(phys:isGhost())
+	dlg["frictionInput"]:setLabel(round(phys:getFriction(), 3))
+	dlg["restitutionInput"]:setLabel(round(phys:getRestitution(), 3))
+	dlg["linearDampingInput"]:setLabel(round(phys:getLinearDamping(), 3))
+	dlg["angularDampingInput"]:setLabel(round(phys:getAngularDamping(), 3))
+	dlg["angularFactorInput"]:setLabel(round(phys:getAngularFactor(), 3))
+	
+	local linearFactor = phys:getLinearFactor()
+	dlg["lfxInput"]:setLabel(round(linearFactor.x, 3))
+	dlg["lfyInput"]:setLabel(round(linearFactor.y, 3))
+	dlg["lfzInput"]:setLabel(round(linearFactor.z, 3))
+  else
+	dlg["massInput"]:setLabel("0")
+	dlg["ghostEnabledCheckButton"]:setValue(false)
+	dlg["frictionInput"]:setLabel(0)
+	dlg["restitutionInput"]:setLabel(0)
+	dlg["linearDampingInput"]:setLabel(0)
+	dlg["angularDampingInput"]:setLabel(0)
+	
+	dlg["angularFactorInput"]:setLabel(1)
+	dlg["lfxInput"]:setLabel(1)
+	dlg["lfyInput"]:setLabel(1)
+	dlg["lfzInput"]:setLabel(1)
+  end
+  
   entityEditor.entity:enableWireframe(true)
-    
+  
   dlg.window:setVisible(true)
   Gui.wm:selectWindow(dlg.window)
   dlg.window:update()
@@ -266,6 +498,33 @@ function updateEntityConfig()
   if newName:len() <= 0 then newName = entityEditor.entity:getName() end
     
   dlg["nameInput"]:setLabel(newName)
+  
+  if dlg["physicsEnabledCheckButton"]:getValue() 
+	and entityEditor.entity:getPhysicsProperties() == nil then
+	
+	entityEditor.entity:createPhysicsProperties()
+	
+  elseif not dlg["physicsEnabledCheckButton"]:getValue() 
+	and entityEditor.entity:getPhysicsProperties() ~= nil then
+		
+	entityEditor.entity:deletePhysicsProperties()
+  end
+  
+  local phys = entityEditor.entity:getPhysicsProperties() 
+  if phys ~= nil then
+	phys:setMass(dlg["massInput"]:getLabel())
+	phys:setGhost(dlg["ghostEnabledCheckButton"]:getValue())
+	
+	phys:setFriction(dlg["frictionInput"]:getLabel())
+	phys:setRestitution(dlg["restitutionInput"]:getLabel())
+	phys:setLinearDamping(dlg["linearDampingInput"]:getLabel())
+	phys:setAngularDamping(dlg["angularDampingInput"]:getLabel())
+	phys:setAngularFactor(dlg["angularFactorInput"]:getLabel())
+	
+	phys:setLinearFactor(NeoLua.Vector3(dlg["lfxInput"]:getLabel(), 
+				dlg["lfyInput"]:getLabel(), dlg["lfzInput"]:getLabel()))
+  end
+  
 end
 
 function entityEditor.updateData()
