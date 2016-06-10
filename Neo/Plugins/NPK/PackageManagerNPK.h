a11d71135b308c6fbfebc7dfd98a3f05554ed2c5
Yannick Pflanzer <yp1995@outlook.com>
2016-06-10 17:02:30 +0200

    + Added NPK plugin

---
+++ Neo/Plugins/NPK/PackageManagerNPK.h
@@ -0,0 +1,74 @@
+//========================================================================
+// Copyright (c) 2012 Philipp Geyer <http://nistur.com>
+//
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgment in the product documentation would
+//    be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such, and must not
+//    be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source
+//    distribution.
+//
+//========================================================================
+
+#ifndef __PACKAGE_MANAGER_NPK_H
+#define __PACKAGE_MANAGER_NPK_H
+
+#include <NeoCore.h>
+
+class PackageFileOpenHook : public Neo::FileOpenHook
+{
+public:
+	Neo::File* open(const char* path, const char* mode);
+};
+
+/*--------------------------------------------------------------------------------
+ * PackageManagerNPK
+ * Package manager with NPK backend
+ * <http://npk.googlecode.com/>
+ *-------------------------------------------------------------------------------*/
+class PackageManagerNPK : public Neo::PackageManager
+{
+private:
+	
+	Neo::Package* m_packages;
+	PackageFileOpenHook* m_fileOpenHook;
+
+	Neo::Package m_writable;
+	
+public:
+
+	PackageManagerNPK();
+	~PackageManagerNPK();
+
+	void init();
+	void cleanup();
+	
+	Neo::Package	loadPackage(const char* packageName);
+	Neo::PackageEnt findEntity(const char* name);
+	void		unloadPackage(Neo::Package package);
+	// for now this will just call unloadNeo::Package
+	// in future it should keep the package in a state
+	// where it can be searched (store a list of the files?)
+	// and then only reloaded when files are requested
+	void		offlinePackage(Neo::Package package);
+
+	// publishing
+	Neo::Package	openPackage(const char* packageName);
+	void		closePackage(Neo::Package package);
+	Neo::PackageEnt addFileToPackage(const char* filename, Neo::Package package, const char* entityName);
+	Neo::Package	mountPackage(Neo::Package package);
+};
+
+#endif
---
+++ Neo/Plugins/NPK/Plugin.cpp
@@ -0,0 +1,23 @@
+#ifdef _WIN32
+	#include <windows.h>
+	extern "C" __declspec(dllexport) void StartPlugin(void);
+	extern "C" __declspec(dllexport) void EndPlugin(void);
+#else
+	extern "C" void StartPlugin(void);
+	extern "C" void EndPlugin(void);
+#endif
+
+#include <NeoEngine.h>
+#include "PackageManagerNPK.h"
+
+static PackageManagerNPK g_packageManager;
+
+void StartPlugin()
+{
+	Neo::NeoEngine::getInstance()->setPackageManager(&g_packageManager);
+}
+
+void EndPlugin()
+{
+
+}
