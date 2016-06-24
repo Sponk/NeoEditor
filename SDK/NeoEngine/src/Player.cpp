2d1b0a68ae98c2c09bd0a5c9f220d3dc226ea088
Yannick Pflanzer <yp1995@outlook.com>
2016-06-24 09:09:25 +0200

    * Fixed handling the quit key in the Player class

--- SDK/NeoEngine/src/Player.cpp
+++ SDK/NeoEngine/src/Player.cpp
@@ -1,3 +1,27 @@
+//========================================================================
+// Copyright (c) 2016 Yannick Pflanzer <www.neo-engine.de>
+//
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgment in the product documentation would
+//    be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such, and must not
+//    be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source
+//    distribution.
+//
+//========================================================================
+
 #include <NeoEngine.h>
 #include <Player.h>
 
@@ -33,8 +57,7 @@ void Player::execute(INPUT_KEYS quitKey)
 	SystemContext* system = m_engine->getSystemContext();
 	InputContext* input = m_engine->getInputContext();
 
-	while(m_engine->isActive()
-		  && !input->isKeyDown(quitKey))
+	while(m_engine->isActive())
 	{
 		unsigned long int time = system->getSystemTick();
 		
@@ -42,6 +65,7 @@ void Player::execute(INPUT_KEYS quitKey)
 		game->draw();
 		
 		system->sleep(m_frameCap - (system->getSystemTick() - time));
+		m_engine->setActive(m_engine->isActive() && (quitKey == KEY_DUMMY || !input->isKeyDown(quitKey)));
 	}
 }
 
